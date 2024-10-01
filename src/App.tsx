f0f02241f62197a4277a9c41b82df34a7fcc0c0e
KatieLisabeth <kat.lisabeth@gmail.com>
2024-10-01 15:03:51 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,39 @@
-import './App.css';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+import Link from '@mui/material/Link';
+import Typography from '@mui/material/Typography';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AppRoutes from 'routes/AppRoutes';
 
-function App() {
+function Copyright() {
   return (
-    <div >
-      <header >
-Hello Readme app
-      </header>
-    </div>
+    <Typography
+      variant="body2"
+      align="center"
+      sx={{
+        color: 'text.secondary',
+      }}
+    >
+      {'Copyright © '}
+      <Link color="inherit" href="https://mui.com/">
+        README Generator
+      </Link>{' '}
+      {new Date().getFullYear()}.
+    </Typography>
   );
 }
 
-export default App;
+export default function App() {
+  return (
+    <>
+      <Container maxWidth="lg">
+        <Box sx={{ my: 3 }}>
+          <Router>
+            <AppRoutes />
+          </Router>
+        </Box>
+        <Copyright />
+      </Container>
+    </>
+  );
+}
