1ef75f5329e5b4d8573750a47968c1a31eed36a1
KatieLisabeth <kat.lisabeth@gmail.com>
2024-10-01 15:03:03 +0200

    add theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,49 @@
-import React from 'react';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import LightModeIcon from '@mui/icons-material/LightMode';
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import { ThemeProvider } from '@mui/material/styles';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { darkTheme, lightTheme } from './theme';
+
+const Root = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(() => {
+    const savedTheme = localStorage.getItem('theme');
+    return savedTheme ? savedTheme === 'dark' : false;
+  });
+
+  useEffect(() => {
+    localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
+  }, [isDarkMode]);
+
+  const handleThemeSwitch = () => {
+    setIsDarkMode((prevMode) => !prevMode);
+  };
+
+  const currentTheme = isDarkMode ? darkTheme : lightTheme;
+
+  return (
+    <ThemeProvider theme={currentTheme}>
+      <CssBaseline />
+      <div style={{ position: 'absolute', top: 16, right: 16 }}>
+        <IconButton onClick={handleThemeSwitch} color="inherit">
+          {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+        </IconButton>
+      </div>
+      <App />
+    </ThemeProvider>
+  );
+};
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>
 );
 
