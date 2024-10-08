3f383f4adf89d99ba402440b3a95e6b77e89abf9
KatieLisabeth <kat.lisabeth@gmail.com>
2024-10-08 15:10:05 +0200

    add context to the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import LightModeIcon from '@mui/icons-material/LightMode';
-import MenuIcon from '@mui/icons-material/Menu';
 import Box from '@mui/material/Box';
 import CssBaseline from '@mui/material/CssBaseline';
 import IconButton from '@mui/material/IconButton';
 import { ThemeProvider } from '@mui/material/styles';
+import { MarkdownProvider } from 'config/Context';
 import Footer from 'navigation/Footer';
 import Sidebar from 'navigation/Sidebar';
 import { useEffect, useState } from 'react';
@@ -18,8 +18,6 @@ export default function App() {
     return savedTheme ? savedTheme === 'dark' : false;
   });
 
-  const [isSidebarExpanded, setIsSidebarExpanded] = useState<boolean>(false);
-
   useEffect(() => {
     localStorage.setItem('theme', isDarkMode ? 'dark' : 'light');
   }, [isDarkMode]);
@@ -28,56 +26,48 @@ export default function App() {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
-  const handleSidebarToggle = () => {
-    setIsSidebarExpanded((prev) => !prev);
-  };
-
   const currentTheme = isDarkMode ? darkTheme : lightTheme;
 
   return (
     <ThemeProvider theme={currentTheme}>
       <CssBaseline />
       <Router>
-        <Box sx={{ display: 'flex', minHeight: '100vh' }}>
-          {/* Sidebar */}
-          <Sidebar isExpanded={isSidebarExpanded} />
+        <MarkdownProvider>
+          <Box sx={{ display: 'flex', minHeight: '100vh' }}>
+            <Sidebar />
 
-          {/* Main Content Area */}
-          <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
-            <Box
-              sx={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignItems: 'center',
-                padding: '.5rem',
-                position: 'relative',
-                zIndex: 10,
-              }}
-            >
-              {/* Sidebar Toggle Button */}
-              <IconButton onClick={handleSidebarToggle} color="inherit">
-                <MenuIcon />
-              </IconButton>
+            {/* Main Content Area */}
+            <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}>
+              <Box
+                sx={{
+                  display: 'flex',
+                  justifyContent: 'flex-end',
+                  alignItems: 'center',
+                  padding: '.5rem',
+                  position: 'relative',
+                  zIndex: 10,
+                }}
+              >
+                {/* Theme Switch Button */}
+                <IconButton onClick={handleThemeSwitch} color="inherit">
+                  {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
+                </IconButton>
+              </Box>
 
-              {/* Theme Switch Button */}
-              <IconButton onClick={handleThemeSwitch} color="inherit">
-                {isDarkMode ? <LightModeIcon /> : <DarkModeIcon />}
-              </IconButton>
+              <Box
+                sx={{
+                  flexGrow: 1,
+                  padding: '1rem',
+                }}
+              >
+                <AppRoutes />
+              </Box>
+
+              {/* Footer */}
+              <Footer />
             </Box>
-
-            <Box
-              sx={{
-                flexGrow: 1,
-                padding: '1rem',
-              }}
-            >
-              <AppRoutes />
-            </Box>
-
-            {/* Footer */}
-            <Footer />
           </Box>
-        </Box>
+        </MarkdownProvider>
       </Router>
     </ThemeProvider>
   );
