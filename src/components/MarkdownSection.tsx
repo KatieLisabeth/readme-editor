89d401084611caebbb58f39f45ad385fc216e0da
KatieLisabeth <kat.lisabeth@gmail.com>
2024-10-10 12:41:03 +0200

    markdown session can work with string array

--- src/components/MarkdownSection.tsx
+++ src/components/MarkdownSection.tsx
@@ -1,11 +1,6 @@
 import { Box, List, ListItem, ListItemText, Typography } from '@mui/material';
 
-interface MarkdownSectionProps {
-  section: ISection | null;
-  onSelectElement: (syntax: string) => void; // Add a callback prop to handle element selection
-}
-
-const MarkdownSection: React.FC<MarkdownSectionProps> = ({
+const MarkdownSection: React.FC<IMarkdownSection> = ({
   section,
   onSelectElement,
 }) => {
@@ -24,10 +19,7 @@ const MarkdownSection: React.FC<MarkdownSectionProps> = ({
       </Typography>
       <List>
         {section.element.map((el) => (
-          <ListItem
-            key={el.id}
-            onClick={() => onSelectElement(el.syntax)} // Handle click event
-          >
+          <ListItem key={el.id} onClick={() => onSelectElement([el.syntax])}>
             <ListItemText primary={el.element} />
           </ListItem>
         ))}
